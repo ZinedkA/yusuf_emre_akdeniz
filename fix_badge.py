11a58932c236f8a6c4888b794b9b40243c1d6845
Yusuf Emre Akdeniz <ysfmrakdeniz@gmail.com>
2026-08-25 17:28:39 +0300

    rev_1.9.6_20260825

---
+++ fix_badge.py
@@ -0,0 +1,9 @@
+﻿import codecs
+
+with open('index.html', 'rb') as f:
+    html = f.read()
+
+html = html.replace(b'https://profile-counter.glitch.me/ysfmrakdeniz.github.io/count.svg', b'https://visitor-badge.laobi.icu/badge?page_id=ysfmrakdeniz.github.io')
+
+with open('index.html', 'wb') as f:
+    f.write(html)
